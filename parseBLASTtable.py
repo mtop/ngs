602ec460baa66efdeba0615b4a5ac8c8a5a26018
mtop <mats@topel.se>
2013-05-22 19:29:27 +0200

    asdf

--- parseBLASTtable.py
+++ parseBLASTtable.py
@@ -3,35 +3,62 @@
 import sys
 
 table = sys.argv[1]
-
 table_file = open(table, "r")
-
 pt_result = {}
 bact_result = {}
+result = {}
 
+# Extract best hit for each gene prediction, and store them in two dictionaries.
 for line in table_file.readlines():
+	if line[:8] == "# Query:":
+		length = line.split()[3]
 	if line[:6] == "contig":
-#		print line.split()[0].replace(",", "" )			# Devel.
+		hit = [line.split()[1]]
+		identity = line.split()[2]
 		if "|ref|" in line.split()[1]:
-			pt_result[line.split()[0].replace(",", "" )] = line.split()[1]
+#			pt_result[line.split()[0].replace(",", "" )] = hit #line.split()[1]
+#			pt_result[line.split()[0].replace(",", "" )].append("Pt")
+			result[line.split()[0].replace(",", "" )] = hit
+			result[line.split()[0].replace(",", "" )].append("Pt")
+			result[line.split()[0].replace(",", "" )].append(identity)
+			result[line.split()[0].replace(",", "" )].append(length)
 		else:
-			bact_result[line.split()[0].replace(",", "" )] = line.split()[1]
-			
-for key in pt_result:
-	print key
-	lineNr = 1
-#	key.split(".")
-	gff_file = key.split(".")[0] + ".gff"
-	print gff_file
-	for line in open(gff_file, "r").readlines():
-		print lineNr, key.split(".")[1]
-		if lineNr == key.split(".")[1]:
-			print line.split()[3], line.split()[4]
-		lineNr =+1
+#			bact_result[line.split()[0].replace(",", "" )] = hit #line.split()[1]
+#			bact_result[line.split()[0].replace(",", "" )].append("B")
+			result[line.split()[0].replace(",", "" )] = hit
+			result[line.split()[0].replace(",", "" )].append("B")
+			result[line.split()[0].replace(",", "" )].append(identity)
+			result[line.split()[0].replace(",", "" )].append(length)
 
-	print key
+
+# Find the start and stop positions of the gene prediction in the Pt contigs.
+for key in result:
+	gff_file = key.split(".")[0] + ".gff"
+	for line in open(gff_file, "r").readlines():
+		# Identify the right line in the *.gff file.
+		if key.split(".")[1] == line.split()[9][5:].replace("\"", ""):
+			start = line.split()[3]
+			stop = line.split()[4]
+#			result[key].append("Pt")
+			result[key].append(start)
+			result[key].append(stop)
+
+
+#for key in bact_result:
+#	gff_file = key.split(".")[0] + ".gff"
+#	for line in open(gff_file, "r").readlines():
+#		# Identify the right line in the *.gff file.
+#		if key.split(".")[1] == line.split()[9][5:].replace("\"", ""):
+#			start = line.split()[3]
+#			stop = line.split()[4]
+#			bact_result[key].append("B")
+#			bact_result[key].append(start)
+#			bact_result[key].append(stop)
+
+
+print "# Contig_nr" + "\t" + "Pt/B" + "\t" + "% Id." + "\t" + "Length" +  "\t" + "Start" + "\t" + "Stop" + "\t" + "Model" + "\t" + "Match"
+for key in sorted(result):
+	print key.split(".")[0] + "\t" + result[key][1] + "\t" + result[key][2] + "\t" + result[key][3] + "\t" + result[key][4] + "\t" + result[key][5] + "\t" + key.split(".")[1] + "\t" + result[key][0]
 
 
 	
-print pt_result
-print bact_result
