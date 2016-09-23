a336fcea87679246da4209adb3ba01e9eb62f708
mtop <mats@topel.se>
2016-09-23 22:40:14 +0200

    Added the 'representative' and 'delimiter' options

--- parseCDHITresult.py
+++ parseCDHITresult.py
@@ -2,6 +2,7 @@
 
 import sys
 
+# Make sure the argparser module is installed (not by default in version <= 2.6). 
 try:
     import argparse
 except ImportError:
@@ -20,10 +21,19 @@ parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbos
 parser.add_argument("cdhitfile", type=str, help="The name of the CH-HIT results file [*.clust] to analyse.")
 parser.add_argument("-c", "--cluster_size", default="1", type=int, help="Return center sequences from clusters of size equal or greater to this value.")
 parser.add_argument("-d", "--sample_delimiter", type=str, help="Indicate the delimiter between sample name and sequence name in the sequence header.")
-parser.add_argument("-r", "--representation", default="1", type=int, help="Return center sequences from clusters with representatives from a specified number of samples.")
-
-
+parser.add_argument("-r", "--representation", default="1", type=int, help="Return center sequences from clusters with representatives from a specified number [or greater] of samples.")
+parser.add_argument("--example", help="Print example commands.")
 args = parser.parse_args()
+
+# Make sure the "--representation" option is accompanied by a "--sample_delimiter" option.
+if args.representation and args.sample_delimiter is None:
+    parser.error("You need to specify [--sample_delimiter] in order to use the [--representation] option.")
+
+#######################################################
+# parseCDHITresult.py my_CD-HIT_results_file.clstr	Prints center sequence headers of all clusters.
+# parseCDHITresult.py my_CD-HIT_results_file.clstr -r 2 -d "_TRINITY_"	Prints center sequence headers of cluster with representatives from atleast two samples, where the sample names are preceded by the string "_TRINITY_" in the sequence header.
+#######################################################
+
 
 
 class Cluster(object):
@@ -31,6 +41,21 @@ class Cluster(object):
 		self.name = cluster
 		self.result = result
 		self.parse_result()
+
+		# If this analysis involvs sequences from several sample, then do the nessesary computation.
+		if args.sample_delimiter:
+			# Calculate the number of samples that are represented in the input file.
+			self.header_list = self.get_results_seq_names()
+			self.sample_list = []
+			# Split the headers at the delimiter and store the first part in the sample list.
+			for header in self.header_list:
+				sample_name = header.split(args.sample_delimiter)[0]
+				if sample_name not in self.sample_list:
+					self.sample_list.append(sample_name)
+		else:
+		# Create an empty list if the sample option was not used. 
+			self.sample_list = [None]
+
 	
 	def get_name(self):
 		return self.name
@@ -62,6 +87,9 @@ class Cluster(object):
 			if number == "REF":
 				return self.r_seq_name_list[i]
 			i += 1
+
+	def get_number_of_samples(self):
+		return len(self.sample_list)
 
 	def parse_result(self):
 		self.r_number_list = []
@@ -100,15 +128,20 @@ def readCDHITtable(infile):
 			result.append(line)
 	if cluster: yield (cluster, ''.join(result))
 
+#def monsterIFstatement():
+#	if int(cluster.get_cluster_size()) >= int(args.cluster_size):
+#		print 
 
 def main():
+	# Parse the CD-HIT input file and extract all information needed for further analyses.
 	clusters = []
 	with open(args.cdhitfile) as infile:
 		for cluster, stuff in readCDHITtable(infile):
 			clusters.append(Cluster(cluster, stuff))
 	
+	# Print the center sequence of all cluster that fullfills the criteria set using the commandline options. This will be amoster style "if" statment...
 	for cluster in clusters:
-		if int(cluster.get_cluster_size()) >= int(args.cluster_size):
+		if int(cluster.get_cluster_size()) >= int(args.cluster_size) and cluster.get_number_of_samples() >= args.representation :
 			print cluster.get_center()
 
 
