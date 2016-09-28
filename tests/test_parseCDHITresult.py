a963acd660bb80cc40a1f1fdd7f7178eed547e9f
mtop <mats@topel.se>
2016-09-28 22:19:49 +0200

    New tests

--- tests/test_parseCDHITresult.py
+++ tests/test_parseCDHITresult.py
@@ -24,11 +24,43 @@ class TestCluster:
 				self.TestClusterClass.append(parseCDHITresult.Cluster(cluster, stuff, self.args))
 		return self.TestClusterClass
 
-	def test_cluster_name(self):
+	def test_name(self):
 		n = 0
 		for cluster in self.populate_the_class():
-			
 			assert cluster.get_name() == ">Cluster %s\n" % n
+			n += 1
+
+#	def test_result(self):
+#		pass
+
+	def test_results_numbers(self):
+		n = 0
+		for cluster in self.populate_the_class():
+			assert cluster.get_results_numbers()[n] == "%s" % n
+			n += 1
+
+	def test_results_length(self):
+		n = 0
+		length = [[1572], [14126, 14200], [249, 8833, 1201], [14979, 1418, 275, 374], [4887, 9334, 724, 14929, 859], [577, 394, 363, 999, 715, 471], [1810, 2310, 1656, 1905, 14519, 1372, 1390], [2512, 2034, 1039, 402, 394, 290, 310, 3932], [616, 1372, 457, 280, 361, 289, 202, 1364, 5361], [12982, 4242, 14316, 11991, 1072, 12678, 8380, 14332, 11940, 14323]]
+		for cluster in self.populate_the_class():
+			for x in range(len(cluster.get_results_length())):
+				assert int(cluster.get_results_length()[x]) == length[n][x]
+			n += 1
+
+	def test_results_seq_names(self):
+		pass
+
+	def test_strains(self):
+		pass
+
+	def test_identities(self):
+		pass
+
+	def test_cluster_size(self):
+		n = 0
+		size = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+		for cluster in self.populate_the_class():
+			assert cluster.get_cluster_size() == size[n]
 			n += 1
 
 	def test_cluster_center(self):
@@ -38,13 +70,6 @@ class TestCluster:
 			assert cluster.get_center() == center[n]
 			n += 1
 
-	def test_cluster_size(self):
-		n = 0
-		size = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-		for cluster in self.populate_the_class():
-			assert cluster.get_cluster_size() == size[n]
-			n += 1
-	
-
-
+	def test_number_of_samples(self):
+		pass
 
